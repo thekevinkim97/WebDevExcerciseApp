f1cd45177c83710a4da2e215f156caec04bb0104
Kevin Kim <BestKevin5897@Kevins-MacBook-Air.local>
2019-10-26 20:11:10 -0400

    added .gitignore (i think), had to add exercise

--- server/models/Exercises.js
+++ server/models/Exercises.js
@@ -1,5 +1,6 @@
 const exercises = [
-    { name: "Bench Press", sets: 3, reps: 5}
+    { name: "Bench Press", sets: 3, reps: 5},
+    { name: "Bicep Curl", sets: 3, reps: 10}
 ];
 
 module.exports = exercises;
